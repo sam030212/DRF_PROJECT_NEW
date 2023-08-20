85d2be4d389923978d09854fd34d1f9378ec05b2
sam030212 <samirnetare@gmail.com>
2023-08-20 04:08:09 -0700

    commit:3

--- watchmate/myappone/views.py
+++ watchmate/myappone/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from myappone.models import Movie
 
 # Create your views here.
 
 def movie_list(request):
     movies = Movie.objects.all()
-    return HttpResponse("This is movie")
+
+    data = {
+        'movies': list(movies.values())
+    }
+    return JsonResponse(data)
